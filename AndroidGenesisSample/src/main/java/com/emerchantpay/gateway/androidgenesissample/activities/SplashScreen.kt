3a488ddb8456a6bfda757926af08cec2cc6abfd6
PetrovP <plamen.petrov@emerchantpay.com>
2019-10-24 15:45:30 +0300

    1.2.1

--- AndroidGenesisSample/src/main/java/com/emerchantpay/gateway/androidgenesissample/activities/SplashScreen.kt
+++ AndroidGenesisSample/src/main/java/com/emerchantpay/gateway/androidgenesissample/activities/SplashScreen.kt
@@ -2,7 +2,7 @@ package com.emerchantpay.gateway.androidgenesissample.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 
 import com.emerchantpay.gateway.androidgenesissample.R
 
